35931513fd6a4525bd2282489edb7ce0993b19bd
deepak-kumar-saini2 <876467deepak@gmail.com>
2026-06-25 17:56:41 +0530

    testing tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='text-3xl font-bold'>App</div>
+    <div className='text-3xl font-bold bg-amber-400'>
+      <h1 className='text-fuchsia-950'>First React app</h1>
+    </div>
   )
 }
 
